7cd5198c2edf7d4fda17315011081127f42fa008
coderguy1777 <hill.jordan@charterschool.org>
2018-10-03 18:00:33 -0400

    Backup of Project for SE.
    Main project is backed up as of 10/3/18

---
+++ Main Project/Assets/CameraTestMovementScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class CameraTestMovementScript : MonoBehaviour
+{
+    int zoom = 40;
+    int normal = 32;
+    float smooth = 5;
+
+    private bool isZoomed = false;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            isZoomed = !isZoomed; //Checks if zoom is true or false, aka when right clicked.
+        }
+
+        if(isZoomed) //if statement for when right clicked.
+        {
+            GetComponent<Camera>().fieldOfView = Mathf.Lerp(GetComponent<Camera>().fieldOfView, zoom, Time.deltaTime * smooth); //Checks for right click and zooms in
+        }
+
+        else
+        {
+            GetComponent<Camera>().fieldOfView = Mathf.Lerp(GetComponent<Camera>().fieldOfView, normal, Time.deltaTime * smooth); //Will zoom out if anything else pops up
+        }
+    }
+}
---
+++ Main Project/Assets/RigidBodyScriptForPineTrees.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidBodyScriptForPineTrees : MonoBehaviour {
+
+    public Rigidbody rb;
+    Rigidbody m_Rigidbody;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+        m_Rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	void PhysicsEnabler()
+    {
+        rb.isKinematic = false;
+        rb.detectCollisions = true;
+    }
+
+    void PhysicsDisabler()
+    {
+        rb.isKinematic = true;
+        rb.detectCollisions = false;
+    }
+
+    void SwitchCollisionDetection()
+    {
+        
+    }
+}
